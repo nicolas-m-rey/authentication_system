9d43db4d93f96bab331e37eeac41c806ccdd647a
Nicolas Rey <nic.m.rey@gmail.com>
2023-01-10 11:52:22 -0800

    Created form, updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages/home'
+  post 'sign_up', to: 'users#create'
+  get 'sign_up', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

169cefa6f294f40d629f187f1d3d8959133d614c
Nicolas Rey <nic.m.rey@gmail.com>
2023-01-10 12:26:44 -0800

    Set up confirmations controller, view, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages/home'
-  post 'sign_up', to: 'users#create'
-  get 'sign_up', to: 'users#new'
+  root "static_pages#home"
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
 end

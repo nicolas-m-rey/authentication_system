3d8a7e39704728c487c4fc6005c6aeb44cf9bebe
Nicolas Rey <nic.m.rey@gmail.com>
2023-01-24 11:06:09 -0800

    Updates to User Mailer & Confirmation Controller

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -15,11 +15,11 @@ class ConfirmationsController < ApplicationController
         @user = User.find_signed(params[:confirmation_token], purpose: :confirm_email)
 
         if @user.present?
-            @user.confirm!
+            if @user.confirm!
             login @user
             redirect_to root_path, notice: "Your account has been confirmed."
         else
-            redirect_to new_confirmation_path, alert: "Invalid token"
+            redirect_to new_confirmation_path, alert: "Something went wrong."
         end
     end
 
